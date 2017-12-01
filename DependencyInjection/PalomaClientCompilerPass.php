640222fda3ac72b7769298edce329c44cc8394ed
Raffaele Sandrini <raffaele.sandrini@origammi.co>
2017-12-01 17:23:52 +0100

    Fix Symfony 2.8 compatibility

--- DependencyInjection/PalomaClientCompilerPass.php
+++ DependencyInjection/PalomaClientCompilerPass.php
@@ -6,23 +6,41 @@ namespace Paloma\ClientBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class PalomaClientCompilerPass implements CompilerPassInterface
 {
 
     public function process(ContainerBuilder $container)
     {
-        // Replace some string arguments with their service equivalence
+        // Replace some string arguments with their service equivalents
         $clientFactoryDef = $container->getDefinition('paloma_client.client_factory');
         // Logger
-        if (($loggerName = $clientFactoryDef->getArgument(3)) !== null) {
-            $logger = $container->getDefinition($loggerName);
-            $clientFactoryDef->setArgument(3, $logger);
-        }
+        $this->replaceReferenceArgument($container, $clientFactoryDef, 3);
         // Cache provider
-        if (($cacheProviderName = $clientFactoryDef->getArgument(7)) !== null) {
-            $cacheProvider = $container->getDefinition($cacheProviderName);
-            $clientFactoryDef->setArgument(7, $cacheProvider);
+        $this->replaceReferenceArgument($container, $clientFactoryDef, 7);
+    }
+
+    /**
+     * This method helps injecting services which have not yet been created into
+     * container references (services which are not yet created). It does this
+     * by replacing the specified constructor argument which has to contain a
+     * string denoting a container service with the actual reference to that
+     * service.
+     * It does so in a Symfony 2.x compatible way.
+     *
+     * @param ContainerBuilder $container
+     * @param Definition $reference
+     * @param int $argIndex
+     */
+    private function replaceReferenceArgument(ContainerBuilder $container,
+        Definition $reference, $argIndex)
+    {
+        $arguments = $reference->getArguments();
+        if ($arguments[$argIndex] !== null) {
+            $replacement = $container->getDefinition($arguments[$argIndex]);
+            $arguments[$argIndex] = $replacement;
+            $reference->setArguments($arguments);
         }
     }
 
