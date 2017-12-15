34c114c6a775c6ec32ea4686cba4f2ffbd8ad253
Raffaele Sandrini <raffaele.sandrini@origammi.co>
2017-12-15 09:56:02 +0100

    Fix Twig cache warming

--- Factory/ClientFactory.php
+++ Factory/ClientFactory.php
@@ -59,7 +59,26 @@ class ClientFactory
      */
     public function getDefaultClient()
     {
+        // We want to have the sanity check in order to find implementation errors
+        // early. However, it is common practice to use a PalomaClient within
+        // Twig extensions. In PROD mode Twig does some cache warmup which results
+        // in Twig extensions being instantiated. If such extensions depend on
+        // the Paloma default client then the method here will be called before
+        // the factory was initialized.
+        // In order to keep the sanity check and still allow the default Paloma
+        // client being used in Twig extensions we perform a check here whether
+        // we are currently in the Symfony cache warmup phase and if so we return
+        // a bogus PalomaClient to not break the cache warmup code.
         if ($this->defaultChannel === null || $this->defaultLocale === null) {
+            $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+            foreach ($bt as $frame) {
+                if (isset($frame['class']) && is_a($frame['class'],
+                        'Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface', true)) {
+                    return $this->getOrCreateClient('symfony_cache_warmup_fake_channel',
+                        'symfony_cache_warmup_fake_locale');
+                }
+            }
+
             throw new \LogicException('Attempt to get the default Paloma client without prior ' .
                 'defining what the default channel and locale is. Forget to call ' .
                 'ClientFactory::setDefaultChannel() or ClientFactory::setDefaultLocale()?');
